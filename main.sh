efa719eec7264a66f5e35092c71587fffd7992cf
kangjw <71870130+kangjwme@users.noreply.github.com>
2021-09-16 18:07:24 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,7 +9,7 @@ export pingms=$(ping -c 10 $IP | grep 'min/avg/max/mdev' | awk '{print $4}' | cu
 export alert=200
 
 
-# 當延遲超過指定職，發送數據
+# 當延遲超過指定值，發送數據
 if [ $(echo | awk '{print int("'$pingms'"*1000)}') -gt $(echo "$alert * 1000" | bc) ] ; then
 	curl -H "Content-Type: application/json" -d "{\"username\":\"網路延遲小精靈\",\"avatar_url\":\"https://thumbs.gfycat.com/ConventionalMiniatureHornedviper-max-1mb.gif\",\"content\":\"\",\"embeds\":[{\"color\":16721408,\"description\":\"德國節點(DE-1) 延遲超過警戒值( $alert ms )，目前為：**$pingms **ms\n期間內可能造成連線卡頓等問題，如果狀況持續請通知 <#864064415828541492>\",\"timestamp\":\"\",\"author\":{\"name\":\"\",\"url\":\"\",\"icon_url\":\"https://thumbs.gfycat.com/ConventionalMiniatureHornedviper-max-1mb.gif\"},\"image\":{\"url\":\"\"},\"thumbnail\":{\"url\":\"\"},\"footer\":{\"text\":\"\"},\"fields\":[]}],\"components\":[]}" $WEBHOOK_URL
 fi
